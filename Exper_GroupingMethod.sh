cf7792c53632cdc7501418e1abcb085d3cdc7b21
jhcknzzm <2352121039@qq.com>
2021-03-18 09:55:18 +0800

    Delete Exper_GroupingMethod.sh

--- Exper_GroupingMethod.sh
+++
@@ -1,6 +0,0 @@
-nohup python train_parallel.py --experiment_num 3  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 0
-nohup python train_parallel.py --experiment_num 17  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 0
-nohup python train_parallel.py --experiment_num 14  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 1
-nohup python train_parallel.py --experiment_num 17  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 1
-nohup python train_parallel.py --experiment_num 0  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 2
-nohup python train_parallel.py --experiment_num 3  --GPU_list 0123467895 --ip_address 128.32.162.169 --datasetid 2
